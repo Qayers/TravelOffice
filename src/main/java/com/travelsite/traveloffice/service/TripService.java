5d094a53d506a5b9afd6e5a9a619696bffb660bd
MKMichalski <mateuszmichalskiK@gmail.com>
2018-07-30 19:36:11 +0200

    added searching trips by city

--- src/main/java/com/travelsite/traveloffice/service/TripService.java
+++ src/main/java/com/travelsite/traveloffice/service/TripService.java
@@ -2,5 +2,9 @@ package com.travelsite.traveloffice.service;
 
 import com.travelsite.traveloffice.model.TripEntity;
 
+import java.util.List;
+
 public interface TripService extends CrudService<TripEntity, Long> {
+
+    List<TripEntity> searchByCity(String city);
 }
