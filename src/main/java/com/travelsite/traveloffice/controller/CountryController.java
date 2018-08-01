d182a70d719af8f6e8a78d34087b3089c1bac737
MKMichalski <mateuszmichalskiK@gmail.com>
2018-08-01 22:53:27 +0200

    change addCountry Controller

--- src/main/java/com/travelsite/traveloffice/controller/CountryController.java
+++ src/main/java/com/travelsite/traveloffice/controller/CountryController.java
@@ -1,11 +1,21 @@
 package com.travelsite.traveloffice.controller;
 
+import com.travelsite.traveloffice.model.ContinentEntity;
 import com.travelsite.traveloffice.model.CountryEntity;
+import com.travelsite.traveloffice.service.ContinentService;
 import com.travelsite.traveloffice.service.CrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -14,6 +24,10 @@ public class CountryController {
     @Autowired
     private CrudService countryService;
 
+    @Autowired
+    @Qualifier("continentServiceImpl")
+    private ContinentService continentService;
+
     @GetMapping("/country")
     public Iterable getCountryName() {
         return countryService.findAll();
@@ -21,8 +35,25 @@ public class CountryController {
 
     @PostMapping("/addCountry")
     public ResponseEntity<CountryEntity> addCountry(@RequestBody CountryEntity countryEntity) {
+
+        ContinentEntity continentEntity = countryEntity.getContinentEntity();
+        Stream<ContinentEntity> continents = iteratorToStream(continentService.findAll().iterator(),true);
+        List<ContinentEntity> list = continents.collect(Collectors.toList());
+        List<ContinentEntity> checkList = list
+                .stream()
+                .filter(e -> e.getName().equals(continentEntity.getName()))
+                .collect(Collectors.toList());
+
+        if (!checkList.isEmpty()) {
+            countryEntity.setContinentEntity(checkList.get(0));
+        }
         countryService.add(countryEntity);
         return ResponseEntity.ok(countryEntity);
+    }
+
+    public static <T> Stream<T> iteratorToStream(final Iterator<T> iterator, final boolean parallell) {
+        Iterable<T> iterable = () -> iterator;
+        return StreamSupport.stream(iterable.spliterator(), parallell);
     }
 
     @GetMapping("/getCountry/{id}")
