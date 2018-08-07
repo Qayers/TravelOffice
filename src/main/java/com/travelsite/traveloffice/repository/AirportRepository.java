40fc1192027b945eaee59817589274dd4d4b6a7a
Andrzej Kulesza <qayers.dev@gmail.com>
2018-08-08 01:44:22 +0200

    add findByCity in AirportService

--- src/main/java/com/travelsite/traveloffice/repository/AirportRepository.java
+++ src/main/java/com/travelsite/traveloffice/repository/AirportRepository.java
@@ -4,6 +4,9 @@ import com.travelsite.traveloffice.model.AirportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AirportRepository extends JpaRepository<AirportEntity, Long> {
+List<AirportEntity>findByCityEntity_Id(Long ForeignKey);
 }
